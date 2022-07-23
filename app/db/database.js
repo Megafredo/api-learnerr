16242b9855de839b33e95a9383724e4caf2450aa
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-07-23 15:29:13 +0200

    [feat] add connexion string

--- app/db/database.js
+++ app/db/database.js
@@ -5,11 +5,12 @@ const logger = debug('Pool');
 //~ Import pg
 import pg from 'pg';
 // Deployement
-// const pool = new pg.Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: true }
-// });
-const client = new pg.Pool();
+const client = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: true }
+});
+// const client = new pg.Pool();
+// console.log("client: ", client);
 
 client.connect()
   .then( () => logger('DB connected') )
