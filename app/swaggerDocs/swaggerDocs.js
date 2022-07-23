c3a227c0ee161741cede6f44236832eff261f87d
Fredo BASLER <megafredo@hotmail.com>
2022-07-23 16:12:57 +0200

    [update] swagger docs

--- app/swaggerDocs/swaggerDocs.js
+++ app/swaggerDocs/swaggerDocs.js
@@ -49,7 +49,7 @@ const options = {
         // Tous les serveurs
         servers: [
             {
-                url: 'https://api-learnerr.herokuapp.com/',
+                url: 'https://api-learnerr.herokuapp.com/api/v1',
                 description: 'API v1'
             },
             {
